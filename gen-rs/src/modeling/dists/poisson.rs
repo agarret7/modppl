93b78d5a44db52cee40a0eb399ddd3bc44654ec8
Austin Garrett <agarrett777@gmail.com>
2024-03-26 02:08:23 -0400

    add: logpdf for Poisson and change output type to i64.

--- gen-rs/src/modeling/dists/poisson.rs
+++ gen-rs/src/modeling/dists/poisson.rs
@@ -6,19 +6,23 @@ use rand_distr::{
 };
 
 
+fn factorial(i: i64) -> i64 {
+    if i <= 1 { 1 } else { i * factorial(i-1) }
+}
+
 /// Poisson distribution type
 pub struct Poisson { }
 
 /// Instantiation of the poisson distribution
 pub const poisson: Poisson = Poisson { };
 
-impl Distribution<f64,f64> for Poisson {
-    fn logpdf(&self, x: &f64, rate: f64) -> f64 {
-        panic!("not implemented");
+impl Distribution<i64,f64> for Poisson {
+    fn logpdf(&self, k: &i64, rate: f64) -> f64 {
+        (*k as f64)*rate.ln() - rate - factorial(*k) as f64
     }
 
-    fn random(&self, rng: &mut ThreadRng, rate: f64) -> f64 {
-        let gamma_sampler = PoissonSampler::new(rate).ok().unwrap();
-        gamma_sampler.sample(rng)
+    fn random(&self, rng: &mut ThreadRng, rate: f64) -> i64 {
+        let poisson_sampler = PoissonSampler::new(rate).ok().unwrap();
+        poisson_sampler.sample(rng) as i64
     }
 }
